7c056b5daca34d51a4e5565dd157b06f94a25e24
Tommaso Rossi <kurtommy@gmail.com>
2017-06-17 23:17:25 +0100

    asd

--- dist/sw.js
+++ dist/sw.js
@@ -19,43 +19,43 @@ importScripts('workbox-sw.prod.v1.0.1.js');
  */
 const fileManifest = [
   {
-    "url": "/404.html",
+    "url": "/albumi/404.html",
     "revision": "a8ac6c99ac3d78bec287b4e15c6ccd82"
   },
   {
-    "url": "/assets/data/artists.json",
+    "url": "/albumi/assets/data/artists.json",
     "revision": "86f1de4ebf89c118cc397670d05236b3"
   },
   {
-    "url": "/assets/lists/names.json",
+    "url": "/albumi/assets/lists/names.json",
     "revision": "55360ec457cf0d0eee4c113e6b30a44a"
   },
   {
-    "url": "/favicon.ico",
+    "url": "/albumi/favicon.ico",
     "revision": "b9aa7c338693424aae99599bec875b5f"
   },
   {
-    "url": "/index.html",
+    "url": "/albumi/index.html",
     "revision": "61ecb62113c51c1a3d023d4d6ac44ec4"
   },
   {
-    "url": "/inline.9c63c12eaa15dd0bf387.bundle.js",
+    "url": "/albumi/inline.9c63c12eaa15dd0bf387.bundle.js",
     "revision": "66138b9861dbc16466ddcb6033b5b07d"
   },
   {
-    "url": "/main.56884db0bfc7932c2662.bundle.js",
+    "url": "/albumi/main.56884db0bfc7932c2662.bundle.js",
     "revision": "d504886cfc2a6632f98442e38e47337a"
   },
   {
-    "url": "/polyfills.39c939ce6d269d8796c3.bundle.js",
+    "url": "/albumi/polyfills.39c939ce6d269d8796c3.bundle.js",
     "revision": "d4be978c966a239dbf98f53b360807b4"
   },
   {
-    "url": "/styles.37cf96475c27c17f27ee.bundle.css",
+    "url": "/albumi/styles.37cf96475c27c17f27ee.bundle.css",
     "revision": "404786c18573fcedf91eb684c532f265"
   },
   {
-    "url": "/vendor.7a5ebf68c96efe95af37.bundle.js",
+    "url": "/albumi/vendor.7a5ebf68c96efe95af37.bundle.js",
     "revision": "906a37e1fb76a9945addff1cd7fe18ae"
   }
 ];
